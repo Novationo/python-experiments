8f33af084db7c463723bfccdfc6529b64571c679
Dylan Smith <dsmit809@gmail.com>
2025-03-26 17:22:16 -0600

    ugh

--- highways.py
+++ highways.py
@@ -2,8 +2,9 @@ interstate = int(input())
 
 if interstate == 0 or 00 or 50 or 60 or 200 or 300 or 400:
     print(f"{interstate} is not a valid interstate highway number.")
+# else:
 
-maininter = interstate // 10
+int(maininter) = int(interstate) // 10
 
 if interstate > 100:
     print(f"{interstate} is an auxilary, serving I-{maininter}")
