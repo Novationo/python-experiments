b2f6f56ad4e5419cf0125036ebb7543d2ea03c05
Dylan / Nova <80179204+Novationo@users.noreply.github.com>
2025-04-14 13:04:01 -0600

    f

---
+++ defexample.py
@@ -0,0 +1,19 @@
+import math # importing dependency
+import madlib # importing existing python file
+from math import sqrt # specific function from other file. 
+
+def someting():
+    print("hi")
+
+someting()
+someting()
+
+if __name__ == "__main__":
+    someting()
+
+# subtasks/functions are very important!
+
+#returns are diferent
+def dude:
+    print("boo")
+    return
